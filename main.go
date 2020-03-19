8e730379ccbdf2e1f53d7e690cf778e963df6f95
samuelvarejames <samuelvarejames@gmail.com>
2020-03-19 23:22:31 +0100

    Update djistra function

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ func main() {
 	//
 }
 
-func dijstra() {
-	// define func
+func dijstra(g []string, n int, s int) []string {
+	// g - adjacency list of a weighted graph
+	// n - the number of nodes in the graph
+	// s - the index of the starting node ( 0 <= s < n )
+
+	return []string{"A"}
 }
