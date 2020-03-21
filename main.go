45c0e68529e1380c8f160051900c06031cf1bbd3
samuelvarejames <samuelvarejames@gmail.com>
2020-03-21 23:42:46 +0100

    Update

--- main.go
+++ main.go
@@ -13,3 +13,12 @@ func dijstra(g []string, n int, s int) []string {
 
 	return []string{"A"}
 }
+
+// this should return the path for points distance covered.
+func findShortestPath() []string {
+	// g - adjacency list of a weighted graph
+	// n - the number of nodes in the graph
+	// s - the index of the starting node ( 0 <= s < n )
+
+	return []string{"A"}
+}
