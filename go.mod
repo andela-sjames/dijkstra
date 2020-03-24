f45a5e8bd49e0128fe12d8bcecabba49084cfebf
samuelvarejames <samuelvarejames@gmail.com>
2020-03-24 21:37:17 +0100

    Pause

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/andela-sjames/dijkstra
+
+go 1.13
+
+require github.com/andela-sjames/priorityQueue v0.0.0-20200315192656-88daddf774c9
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	pqueue "github.com/andela-sjames/priorityQueue"
+)
+
 func main() {
 }
 
@@ -7,6 +11,12 @@ func dijstra(g []string, n int, s int) []string {
 	// g - adjacency list of a weighted graph
 	// n - the number of nodes in the graph
 	// s - the index of the starting node ( 0 <= s < n )
+	visited := make([]bool, n)
+	distance := make([]int, n)
+
+	// Will hack this by using a negative number
+	// to make this pq a min Heap pq.
+	minpq = pqueue.NewHeap()
 
 	// write sudo code here
 
