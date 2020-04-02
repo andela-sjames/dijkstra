632a642b9d6ca84984b384b9ff399dbb2ff2a7f3
samuelvarejames <samuelvarejames@gmail.com>
2020-04-02 23:14:50 +0200

    update

--- main.go
+++ main.go
@@ -8,9 +8,26 @@ import (
 )
 
 func main() {
+	adjList := [][]int{
+		{1, 6, 8},
+		{0, 4, 6, 9},
+		{4, 6},
+		{4, 5, 8},
+		{1, 2, 3, 5, 9},
+		{3, 4},
+		{0, 1, 2},
+		{8, 9},
+		{0, 3, 7},
+		{1, 4, 7},
+	}
+
+	n := 10
+	s := 0
+
+	dijstra(adjList, n, s)
 }
 
-func dijstra(g []string, n int, s int) []string {
+func dijstra(g [][]int, n int, s int) []string {
 	// g - adjacency list of a weighted graph
 	// n - the number of nodes in the graph
 	// s - the index of the starting node ( 0 <= s < n )
