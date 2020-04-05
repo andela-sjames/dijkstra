94da8c7242ff83f1834884d00c583e01b8f0254f
samuelvarejames <samuelvarejames@gmail.com>
2020-04-05 23:33:18 +0200

    update

--- main.go
+++ main.go
@@ -11,17 +11,12 @@ import (
 func main() {
 
 	// create a proper adj list
+	//
 	adjList := [][]int{
 		{1, 6, 8},
 		{0, 4, 6, 9},
 		{4, 6},
 		{4, 5, 8},
-		{1, 2, 3, 5, 9},
-		{3, 4},
-		{0, 1, 2},
-		{8, 9},
-		{0, 3, 7},
-		{1, 4, 7},
 	}
 
 	n := 10
@@ -29,6 +24,12 @@ func main() {
 
 	res := dijstra(adjList, n, s)
 	fmt.Println(res)
+}
+
+type node struct {
+	data   int
+	weight int
+	next   *node
 }
 
 func dijstra(g [][]int, n int, s int) []string {
